2948fbf3afa95a8c48569cc3d2ddf24892fadeb1
0megq <nvatanshenas55@gmail.com>
2024-03-10 19:15:30 -0700

    Obstacle scaling in tornadoes +Adjusted torque
    - Torque exponent is now a variable
    - Added comments to tornado script
    - Pulled obstacles are scaled based off their proximity to the center of the tornado
    - Replaced all "object" with "obstacle" in tornado script to unify naming
    - Tornado spins objects clockwise

--- scripts/midwest/tornado.gd
+++ scripts/midwest/tornado.gd
@@ -4,15 +4,26 @@ extends Area2D
 ## Speed in pixels
 @export var path_follow_speed: float
 
-## The value that the linear velocity is multiplied by to get the impulse that is applied on the body when the tornado releases that body
+## Multiplier of force applied tangent to the vector between the tornado center and obstacle.
 @export var force_vel_multiplier: float = 1.0
+
+## Minimum scale of obstacles for obstacles in tornado. obstacles are scaled between this value and 1
+@export_range(0,1) var minimum_scale: float = 0.5
+
+## Multiplier of (velocity.length_squared() ^ torque_vel_exponent). Changes how much overall torque is applied. Applied after torque_vel_exponent.
 @export var torque_vel_multiplier: float = 20
-## Time before an additional force is applied on the object (in ms)
+
+## Exponent that is put on the magnitude of the linear velocity squared. Used in conjunction with torque_vel_multiplier. Change how much velocity impacts the torque. Applied before torque_vel_multiplier.
+@export var torque_vel_exponent: float = 0.4
+
+## Time before an additional force is applied on the obstacle (in ms)
 @export var time_before_force: float = 1000
 
 # Pixel offset
 var current_path_offset: float = 0
-var pulled_objects: Array[PulledObstacle]
+var pulled_obstacles: Array[PulledObstacle]
+
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 
 #func _ready() -> void:
@@ -24,23 +35,32 @@ func _physics_process(delta: float) -> void:
 	if path:
 		current_path_offset += path_follow_speed * delta
 		global_position = path.curve.sample_baked(current_path_offset) + path.global_position
-	for object in pulled_objects:
-		object.body.apply_torque(object.body.linear_velocity.length() * torque_vel_multiplier)
-		if object.get_time_since_pulled() > time_before_force:
-			var perpindicular_to_tangent: Vector2 = global_position.direction_to(object.body.global_position)
+	for obstacle in pulled_obstacles:
+		# Scale based on proximity to tornado
+		var dist := global_position.distance_to(obstacle.body.global_position)
+		obstacle.body.set_obstacle_scale(remap(min(dist / collision_shape.shape.radius, 1), 0, 1, minimum_scale, 1))
+		
+		
+		# Rotate obstacle based on its velocity. The decimal number
+		obstacle.body.apply_torque(obstacle.body.linear_velocity.length_squared() ** torque_vel_exponent * torque_vel_multiplier)
+		# Apply an extra force
+		if obstacle.get_time_since_pulled() > time_before_force:
+			var perpindicular_to_tangent: Vector2 = global_position.direction_to(obstacle.body.global_position)
 			var tangent: Vector2 = perpindicular_to_tangent.rotated(PI / 2) # rotate that by 90 to get tangent
-			object.body.apply_central_force(tangent * object.body.linear_velocity.length() * force_vel_multiplier)
+			obstacle.body.apply_central_force(tangent * obstacle.body.linear_velocity.length() * force_vel_multiplier)
+		
 
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is RigidBody2D:
-		pulled_objects.append(PulledObstacle.new(body))
+		pulled_obstacles.append(PulledObstacle.new(body))
 
 
 func _on_body_exited(body: Node2D) -> void:
-	for i in pulled_objects.size():
-		if pulled_objects[i].body == body:
-			pulled_objects.remove_at(i)
+	for i in pulled_obstacles.size():
+		if pulled_obstacles[i].body == body:
+			pulled_obstacles[i].body.set_obstacle_scale(1)
+			pulled_obstacles.remove_at(i)
 			return
 
 
