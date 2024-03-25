42aab808d703ef6658b95ea3965d2a99ddc40da4
Sean <seanzeng77@gmail.com>
2024-03-25 15:04:33 -0700

    obstacles
    added basic obstacles

--- scripts/niagara-falls/niagara-falls-minigame.gd
+++ scripts/niagara-falls/niagara-falls-minigame.gd
@@ -1,4 +1,28 @@
 extends BaseMinigame
 
+const SPAWN_AMOUNT: int = 100
+
+const top_left_bound: Vector2 = Vector2(0, 0)
+const bot_right_bound: Vector2 = Vector2(1152, 648)
+
+var spawn_delay: float = 2
+var spawn_delay_reduction: float = 0.1
+
+func _ready() -> void:
+	randomize()
+	for i in range(SPAWN_AMOUNT):
+		_spawn_obstacle()
+		await _wait(spawn_delay)
+		spawn_delay -= spawn_delay_reduction
+
+func _spawn_obstacle() -> void:
+	var obstacle = preload("res://scenes/niagara-falls/obstacle.tscn").instantiate()
+	var rand_x = randf_range(top_left_bound.x, bot_right_bound.x)
+	obstacle.global_position = Vector2(rand_x, -200)
+	add_child(obstacle)
+	
 func _on_player_lost() -> void:
 	end_minigame(false, 0)
+	
+func _wait(seconds: float) -> void:
+	await get_tree().create_timer(seconds).timeout
--- scripts/niagara-falls/player.gd
+++ scripts/niagara-falls/player.gd
@@ -1,41 +1,43 @@
-extends CharacterBody2D
+extends Area2D
 
 signal player_killed
 
-const MAX_SPEED: Vector2 = Vector2(400, 400)
-const ACCELERATION: Vector2 = MAX_SPEED * 10
+const MAX_SPEED: float = 400
+const MAX_FALL_SPEED: float = 700
+const ACCELERATION: float = MAX_SPEED * 10
 const AUTOSCROLL_SPEED: float = 600
 const MIN_SCALE: float = 0.3
 
 const top_left_bound: Vector2 = Vector2(0, 0)
-const bot_right_bound: Vector2 = Vector2(1920, 1080)
+const bot_right_bound: Vector2 = Vector2(1152, 648)
+
+var velocity: Vector2 = Vector2(0, 0)
+var dead: bool
 
 func _physics_process(delta: float) -> void:
 	move(delta)
 
 func move(delta: float) -> void:
+	if dead:
+		return
+	
 	var input := Input.get_vector("left", "right", "up", "down")
 	
-	# Horizontal
-	if input.x: # Acceleration
-		velocity.x += input.x * ACCELERATION.x * delta
-	elif velocity.x != 0: # Deacceleration
-		if absf(velocity.x) < ACCELERATION.x * delta:
+	if input.x:
+		velocity.x += input.x * ACCELERATION * delta
+	elif velocity.x != 0: 
+		if absf(velocity.x) < ACCELERATION * delta:
 			velocity.x = 0
 		else:
-			velocity.x += -signf(velocity.x) * ACCELERATION.x * delta
+			velocity.x += -signf(velocity.x) * ACCELERATION * delta
 
-	# Capping speed
-	velocity.x = clampf(velocity.x, -MAX_SPEED.x, MAX_SPEED.x)
+	velocity.x = clampf(velocity.x, -MAX_SPEED, MAX_SPEED)
+	#velocity = velocity.move_toward(velocity.normalized() * MAX_SPEED, sand_acceleration * 2 * delta)
 
-	# Basic movement. Adding back AUTOSCROLL_SPEED
-	move_and_collide(velocity * delta)
-	
-	# Clamping position
+	position.x += velocity.x * delta
 	position.x = clampf(position.x, top_left_bound.x, bot_right_bound.x)
 	# position.y = clampf(position.y, top_left_bound.global_position.y, bot_right_bound.global_position.y)
 	
-
 # Obstacle
-func _on_body_entered(body: Node2D) -> void:
+func _on_area_entered(body: Node2D) -> void:
 	print("test")
