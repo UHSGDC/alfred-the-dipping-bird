11918606a7940c65e3055956fa69f97073ee09df
0megq <nvatanshenas55@gmail.com>
2024-04-12 22:14:09 -0700

    Added weak spot animation and weak spot signals and counters for later usage

--- scenes/icy/weak_spot.gd
+++ scenes/icy/weak_spot.gd
@@ -1,5 +1,23 @@
 extends Area2D
 
+signal cracked_set(value: bool)
+
+var cracked: bool = false :
+	set(value):
+		cracked = value
+		cracked_set.emit(value)
+
+@onready var sprite: Sprite2D = $Sprite2D
 
 func crack() -> void:
-	print("cracking the weak spot")
+	if sprite.frame == 1:
+		cracked = true
+	if sprite.frame < 2:
+		sprite.frame += 1
+
+
+func freeze_over() -> void:
+	if sprite.frame == 2:
+		cracked = false
+	if sprite.frame > 0:
+		sprite.frame -= 1
---
+++ scenes/icy/weak_spots.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+signal uncracked_count_changed(new_value: int)
+
+var uncracked_count: int :
+	set(value):
+		uncracked_count_changed.emit(value)
+		uncracked_count = value
+		
+
+func _ready() -> void:
+	uncracked_count = get_child_count()
+	for child in get_children():
+		child.cracked_set.connect(_on_cracked_set)
+		
+
+func _on_cracked_set(value: bool) -> void:
+	uncracked_count -= int(value)
