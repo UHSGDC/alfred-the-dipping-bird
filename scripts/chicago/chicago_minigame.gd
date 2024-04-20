aaba83ff5b6526475a1577bbdee36693dfef8b7e
Nick '0megq' Vatanshenas <nvatanshenas55@gmail.com>
2024-04-20 11:31:11 -0700

    Final edits to chicago minigame
    - everythign is done aside from the starting fade in, sounds, and levels

--- scripts/chicago/chicago_minigame.gd
+++ scripts/chicago/chicago_minigame.gd
@@ -1,7 +1,26 @@
 extends BaseMinigame
 
-func _on_player_lost() -> void:
+func _ready() -> void:
+	$HUD/ProgressBar.max_value = $WinArea.position.x
+
+
+func _on_player_player_killed() -> void:
 	end_minigame(false, 0)
 
-func _on_player_won(lives: int) -> void:
-	end_minigame(true, lives)
+
+func _on_win_area_area_entered(area: Area2D) -> void:
+	if !area.is_in_group("player"):
+		return
+		
+	$HUD/BlackScreen.fade_in()
+	await $HUD/BlackScreen.fade_done
+	end_minigame(true, $Player.current_lives)
+
+
+func _process(_delta: float) -> void:
+	$HUD/ProgressBar.value = $Player.position.x
+
+
+func _on_player_lives_changed(new_val: int, max_val: int) -> void:
+	$HUD/LivesBar.value = new_val
+	$HUD/LivesBar.max_value = max_val
