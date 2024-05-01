b580e10570e6f90192e9c58d161388d294f1d6c6
0megq <nvatanshenas55@gmail.com>
2024-04-30 19:03:27 -0700

    Options menu done aside from adding all options

--- scripts/menus/options_menu.gd
+++ scripts/menus/options_menu.gd
@@ -1,6 +1,18 @@
 extends BaseMenu
 
 
-func _on_back_pressed() -> void:
+func _back_input() -> void:
+	print("yo")
+	if $ControlsView.visible:
+		$ControlsView.hide()
+		get_tree().set_input_as_handled()
+		return
+		
+	super()
+
+func _on_back_pressed() -> void:	
 	menus.change_menu(menus.previous_menu, false)
 	
+
+func _on_view_controls_pressed() -> void:
+	$ControlsView.show()
